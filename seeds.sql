21a351aee747cb76eff8c84b2a592429d4e4c983
Sydney Krantz <syd107@gmail.com>
2020-04-13 21:40:41 -0700

    adding handlebars

---
+++ seeds.sql
@@ -0,0 +1,99 @@
+INSERT INTO books (title, author, publishDate, summary, coverArtURL, reviewURL, weekID) VALUES (
+'Where The Crawdads Sing',
+'Delia Owens',
+ 'Aug 14, 2018',
+ 'For years, rumors of the "Marsh Girl" have haunted Barkley Cove, a quiet town on the North Carolina coast. So in late 1969, when handsome Chase Andrews is found dead, the locals immediately suspect Kya Clark, the so-called Marsh Girl. But Kya is not what they say. Sensitive and intelligent, she has survived for years alone in the marsh that she calls home, finding friends in the gulls and lessons in the sand. Then the time comes when she yearns to be touched and loved. When two young men from town become intrigued by her wild beauty, Kya opens herself to a new life--until the unthinkable happens.',
+ 'https://books.google.com/books/content?id=CGVDDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71XDbsqXJIGtxKOi6UtWQzAWklf-fnPG6ZSgefNVcusOVmOKTsmJOh3K-4kmdDmtQxuz1Dy-4SHLO1YiVNz2aYGP_ehNLx0n9eUBaUr1WHfsmz7UMiE3tjeggT9zo8SaEkrihx6',
+ 'https://books.google.com/books?id=CGVDDwAAQBAJ&newbks=1&newbks_redir=0&dq=nyt+best+books+2020&source=gbs_navlinks_s',
+ 1
+ );
+ INSERT INTO books (title, author, publishDate, summary, coverArtURL, reviewURL, weekID) VALUES (
+'Catch and Kill: Lies, Spies, and a Conspiracy to Protect Predators',
+ 'Aug 14, 2018',
+ 'Delia Owens',
+ 'For years, rumors of the "Marsh Girl" have haunted Barkley Cove, a quiet town on the North Carolina coast. So in late 1969, when handsome Chase Andrews is found dead, the locals immediately suspect Kya Clark, the so-called Marsh Girl. But Kya is not what they say. Sensitive and intelligent, she has survived for years alone in the marsh that she calls home, finding friends in the gulls and lessons in the sand. Then the time comes when she yearns to be touched and loved. When two young men from town become intrigued by her wild beauty, Kya opens herself to a new life--until the unthinkable happens.',
+ 'https://books.google.com/books/content?id=CGVDDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71XDbsqXJIGtxKOi6UtWQzAWklf-fnPG6ZSgefNVcusOVmOKTsmJOh3K-4kmdDmtQxuz1Dy-4SHLO1YiVNz2aYGP_ehNLx0n9eUBaUr1WHfsmz7UMiE3tjeggT9zo8SaEkrihx6',
+ 'https://books.google.com/books?id=CGVDDwAAQBAJ&newbks=1&newbks_redir=0&dq=nyt+best+books+2020&source=gbs_navlinks_s',
+ 1
+ );
+ INSERT INTO books (title, author, publishDate, summary, coverArtURL, reviewURL, weekID) VALUES (
+'Where The Crawdads Sing',
+ 'Aug 14, 2018',
+ 'Delia Owens',
+ 'For years, rumors of the "Marsh Girl" have haunted Barkley Cove, a quiet town on the North Carolina coast. So in late 1969, when handsome Chase Andrews is found dead, the locals immediately suspect Kya Clark, the so-called Marsh Girl. But Kya is not what they say. Sensitive and intelligent, she has survived for years alone in the marsh that she calls home, finding friends in the gulls and lessons in the sand. Then the time comes when she yearns to be touched and loved. When two young men from town become intrigued by her wild beauty, Kya opens herself to a new life--until the unthinkable happens.',
+ 'https://books.google.com/books/content?id=CGVDDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71XDbsqXJIGtxKOi6UtWQzAWklf-fnPG6ZSgefNVcusOVmOKTsmJOh3K-4kmdDmtQxuz1Dy-4SHLO1YiVNz2aYGP_ehNLx0n9eUBaUr1WHfsmz7UMiE3tjeggT9zo8SaEkrihx6',
+ 'https://books.google.com/books?id=CGVDDwAAQBAJ&newbks=1&newbks_redir=0&dq=nyt+best+books+2020&source=gbs_navlinks_s',
+ 1
+ );
+ INSERT INTO books (title, author, publishDate, summary, coverArtURL, reviewURL, weekID) VALUES (
+'Where The Crawdads Sing',
+ 'Aug 14, 2018',
+ 'Delia Owens',
+ 'For years, rumors of the "Marsh Girl" have haunted Barkley Cove, a quiet town on the North Carolina coast. So in late 1969, when handsome Chase Andrews is found dead, the locals immediately suspect Kya Clark, the so-called Marsh Girl. But Kya is not what they say. Sensitive and intelligent, she has survived for years alone in the marsh that she calls home, finding friends in the gulls and lessons in the sand. Then the time comes when she yearns to be touched and loved. When two young men from town become intrigued by her wild beauty, Kya opens herself to a new life--until the unthinkable happens.',
+ 'https://books.google.com/books/content?id=CGVDDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71XDbsqXJIGtxKOi6UtWQzAWklf-fnPG6ZSgefNVcusOVmOKTsmJOh3K-4kmdDmtQxuz1Dy-4SHLO1YiVNz2aYGP_ehNLx0n9eUBaUr1WHfsmz7UMiE3tjeggT9zo8SaEkrihx6',
+ 'https://books.google.com/books?id=CGVDDwAAQBAJ&newbks=1&newbks_redir=0&dq=nyt+best+books+2020&source=gbs_navlinks_s',
+ 1
+ );
+ INSERT INTO books (title, author, publishDate, summary, coverArtURL, reviewURL, weekID) VALUES (
+'Where The Crawdads Sing',
+ 'Aug 14, 2018',
+ 'Delia Owens',
+ 'For years, rumors of the "Marsh Girl" have haunted Barkley Cove, a quiet town on the North Carolina coast. So in late 1969, when handsome Chase Andrews is found dead, the locals immediately suspect Kya Clark, the so-called Marsh Girl. But Kya is not what they say. Sensitive and intelligent, she has survived for years alone in the marsh that she calls home, finding friends in the gulls and lessons in the sand. Then the time comes when she yearns to be touched and loved. When two young men from town become intrigued by her wild beauty, Kya opens herself to a new life--until the unthinkable happens.',
+ 'https://books.google.com/books/content?id=CGVDDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71XDbsqXJIGtxKOi6UtWQzAWklf-fnPG6ZSgefNVcusOVmOKTsmJOh3K-4kmdDmtQxuz1Dy-4SHLO1YiVNz2aYGP_ehNLx0n9eUBaUr1WHfsmz7UMiE3tjeggT9zo8SaEkrihx6',
+ 'https://books.google.com/books?id=CGVDDwAAQBAJ&newbks=1&newbks_redir=0&dq=nyt+best+books+2020&source=gbs_navlinks_s',
+ 1
+ );
+ INSERT INTO books (title, author, publishDate, summary, coverArtURL, reviewURL, weekID) VALUES (
+'Where The Crawdads Sing',
+ 'Aug 14, 2018',
+ 'Delia Owens',
+ 'For years, rumors of the "Marsh Girl" have haunted Barkley Cove, a quiet town on the North Carolina coast. So in late 1969, when handsome Chase Andrews is found dead, the locals immediately suspect Kya Clark, the so-called Marsh Girl. But Kya is not what they say. Sensitive and intelligent, she has survived for years alone in the marsh that she calls home, finding friends in the gulls and lessons in the sand. Then the time comes when she yearns to be touched and loved. When two young men from town become intrigued by her wild beauty, Kya opens herself to a new life--until the unthinkable happens.',
+ 'https://books.google.com/books/content?id=CGVDDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71XDbsqXJIGtxKOi6UtWQzAWklf-fnPG6ZSgefNVcusOVmOKTsmJOh3K-4kmdDmtQxuz1Dy-4SHLO1YiVNz2aYGP_ehNLx0n9eUBaUr1WHfsmz7UMiE3tjeggT9zo8SaEkrihx6',
+ 'https://books.google.com/books?id=CGVDDwAAQBAJ&newbks=1&newbks_redir=0&dq=nyt+best+books+2020&source=gbs_navlinks_s',
+ 1
+ );
+ INSERT INTO books (title, author, publishDate, summary, coverArtURL, reviewURL, weekID) VALUES (
+'Where The Crawdads Sing',
+ 'Aug 14, 2018',
+ 'Delia Owens',
+ 'For years, rumors of the "Marsh Girl" have haunted Barkley Cove, a quiet town on the North Carolina coast. So in late 1969, when handsome Chase Andrews is found dead, the locals immediately suspect Kya Clark, the so-called Marsh Girl. But Kya is not what they say. Sensitive and intelligent, she has survived for years alone in the marsh that she calls home, finding friends in the gulls and lessons in the sand. Then the time comes when she yearns to be touched and loved. When two young men from town become intrigued by her wild beauty, Kya opens herself to a new life--until the unthinkable happens.',
+ 'https://books.google.com/books/content?id=CGVDDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71XDbsqXJIGtxKOi6UtWQzAWklf-fnPG6ZSgefNVcusOVmOKTsmJOh3K-4kmdDmtQxuz1Dy-4SHLO1YiVNz2aYGP_ehNLx0n9eUBaUr1WHfsmz7UMiE3tjeggT9zo8SaEkrihx6',
+ 'https://books.google.com/books?id=CGVDDwAAQBAJ&newbks=1&newbks_redir=0&dq=nyt+best+books+2020&source=gbs_navlinks_s',
+ 1
+ );
+ INSERT INTO books (title, author, publishDate, summary, coverArtURL, reviewURL, weekID) VALUES (
+'Where The Crawdads Sing',
+ 'Aug 14, 2018',
+ 'Delia Owens',
+ 'For years, rumors of the "Marsh Girl" have haunted Barkley Cove, a quiet town on the North Carolina coast. So in late 1969, when handsome Chase Andrews is found dead, the locals immediately suspect Kya Clark, the so-called Marsh Girl. But Kya is not what they say. Sensitive and intelligent, she has survived for years alone in the marsh that she calls home, finding friends in the gulls and lessons in the sand. Then the time comes when she yearns to be touched and loved. When two young men from town become intrigued by her wild beauty, Kya opens herself to a new life--until the unthinkable happens.',
+ 'https://books.google.com/books/content?id=CGVDDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71XDbsqXJIGtxKOi6UtWQzAWklf-fnPG6ZSgefNVcusOVmOKTsmJOh3K-4kmdDmtQxuz1Dy-4SHLO1YiVNz2aYGP_ehNLx0n9eUBaUr1WHfsmz7UMiE3tjeggT9zo8SaEkrihx6',
+ 'https://books.google.com/books?id=CGVDDwAAQBAJ&newbks=1&newbks_redir=0&dq=nyt+best+books+2020&source=gbs_navlinks_s',
+ 1
+ );
+ INSERT INTO books (title, author, publishDate, summary, coverArtURL, reviewURL, weekID) VALUES (
+'Where The Crawdads Sing',
+ 'Aug 14, 2018',
+ 'Delia Owens',
+ 'For years, rumors of the "Marsh Girl" have haunted Barkley Cove, a quiet town on the North Carolina coast. So in late 1969, when handsome Chase Andrews is found dead, the locals immediately suspect Kya Clark, the so-called Marsh Girl. But Kya is not what they say. Sensitive and intelligent, she has survived for years alone in the marsh that she calls home, finding friends in the gulls and lessons in the sand. Then the time comes when she yearns to be touched and loved. When two young men from town become intrigued by her wild beauty, Kya opens herself to a new life--until the unthinkable happens.',
+ 'https://books.google.com/books/content?id=CGVDDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71XDbsqXJIGtxKOi6UtWQzAWklf-fnPG6ZSgefNVcusOVmOKTsmJOh3K-4kmdDmtQxuz1Dy-4SHLO1YiVNz2aYGP_ehNLx0n9eUBaUr1WHfsmz7UMiE3tjeggT9zo8SaEkrihx6',
+ 'https://books.google.com/books?id=CGVDDwAAQBAJ&newbks=1&newbks_redir=0&dq=nyt+best+books+2020&source=gbs_navlinks_s',
+ 1
+ );
+ INSERT INTO books (title, author, publishDate, summary, coverArtURL, reviewURL, weekID) VALUES (
+'Where The Crawdads Sing',
+ 'Aug 14, 2018',
+ 'Delia Owens',
+ 'For years, rumors of the "Marsh Girl" have haunted Barkley Cove, a quiet town on the North Carolina coast. So in late 1969, when handsome Chase Andrews is found dead, the locals immediately suspect Kya Clark, the so-called Marsh Girl. But Kya is not what they say. Sensitive and intelligent, she has survived for years alone in the marsh that she calls home, finding friends in the gulls and lessons in the sand. Then the time comes when she yearns to be touched and loved. When two young men from town become intrigued by her wild beauty, Kya opens herself to a new life--until the unthinkable happens.',
+ 'https://books.google.com/books/content?id=CGVDDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71XDbsqXJIGtxKOi6UtWQzAWklf-fnPG6ZSgefNVcusOVmOKTsmJOh3K-4kmdDmtQxuz1Dy-4SHLO1YiVNz2aYGP_ehNLx0n9eUBaUr1WHfsmz7UMiE3tjeggT9zo8SaEkrihx6',
+ 'https://books.google.com/books?id=CGVDDwAAQBAJ&newbks=1&newbks_redir=0&dq=nyt+best+books+2020&source=gbs_navlinks_s',
+ 1
+ );
+ INSERT INTO books (title, author, publishDate, summary, coverArtURL, reviewURL, weekID) VALUES (
+'Where The Crawdads Sing',
+ 'Aug 14, 2018',
+ 'Delia Owens',
+ 'For years, rumors of the "Marsh Girl" have haunted Barkley Cove, a quiet town on the North Carolina coast. So in late 1969, when handsome Chase Andrews is found dead, the locals immediately suspect Kya Clark, the so-called Marsh Girl. But Kya is not what they say. Sensitive and intelligent, she has survived for years alone in the marsh that she calls home, finding friends in the gulls and lessons in the sand. Then the time comes when she yearns to be touched and loved. When two young men from town become intrigued by her wild beauty, Kya opens herself to a new life--until the unthinkable happens.',
+ 'https://books.google.com/books/content?id=CGVDDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71XDbsqXJIGtxKOi6UtWQzAWklf-fnPG6ZSgefNVcusOVmOKTsmJOh3K-4kmdDmtQxuz1Dy-4SHLO1YiVNz2aYGP_ehNLx0n9eUBaUr1WHfsmz7UMiE3tjeggT9zo8SaEkrihx6',
+ 'https://books.google.com/books?id=CGVDDwAAQBAJ&newbks=1&newbks_redir=0&dq=nyt+best+books+2020&source=gbs_navlinks_s',
+ 1
+ );
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
+var exphbs = require("express-handlebars");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 
@@ -12,11 +13,15 @@ var db = require("./models");
 var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+// handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
