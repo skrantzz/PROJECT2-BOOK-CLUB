d5ea9b6cb8e9aae7e9a6011559937afc1c85d87d
Amber Outman <amberoutman@yahoo.com>
2020-04-14 19:25:36 -0700

    added ambers files

---
+++ models/bookOfTheWeek.js
@@ -0,0 +1,21 @@
+module.exports = function(sequelize, DataTypes) {
+    var BookOfTheWeek = sequelize.define('BookOfTheWeek',{
+        wid: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        bid: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false
+        },
+        weekStart:{
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+            unique: true
+        }
+
+    });
+    return BookOfTheWeek
+}
